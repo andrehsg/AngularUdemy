4a08e762114f62ce36b3834931491b54c7510e28
andrehsg <andrehsg@hotmail.com>
2018-06-15 17:08:03 -0300

    test

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -2,14 +2,14 @@ import { Component } from '@angular/core';
 
 @Component ({
 selector: 'app-server',
-templateUrl: './server.component.html'	
-styleUrls: ['./app.component.css']
+templateUrl: './server.component.html',	
+styleUrls: ['./../app.component.css']
 	
 })
 
 export class ServerComponent {
 	
-title = 'app';
+title = 'Server';
 
 	
 	
