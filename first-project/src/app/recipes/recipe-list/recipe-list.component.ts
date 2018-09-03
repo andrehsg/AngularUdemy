299080114ceef969eddd88adfdd355c8f93651c3
andre <andrehsg@hotmail.com>
2018-09-02 21:05:32 -0700

    0902

---
+++ first-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+receitas: Recipe[] = [ new Recipe('A test recipe', 'Some ingredients' , 'https://i.ytimg.com/vi/jPOxWOE-3Xk/maxresdefault.jpg')
+];
+  constructor() {
+  this.receitas.push(new Recipe('A tests rsecipe', 'Some ingredients' , 'https://i.ytimg.com/vi/jPOxWOE-3Xk/maxresdefault.jpg')); }
+
+  ngOnInit() {
+  }
+
+}
